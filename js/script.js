29ab776a6ca541487bdb485e1a2f07a6bcfb85a3
Laura Bigoni <laurabigoni.92@gmail.com>
2022-02-01 12:47:47 +0100

    added class active and showing messages

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ Vue.config.devtools = true;
 const root = new Vue({
     el: '#root',
     data: {
+        currentIndex: 0,
         user: {
             name: 'Laura Bigoni',
             avatar: '_io'
@@ -12,26 +13,87 @@ const root = new Vue({
             {
                 name: 'Michele',
                 avatar: '_1',
-                active: true,
+                visible: true,
+                messages: [{
+                    date: '10/01/2020 15:30:55',
+                    text: 'Hai portato a spasso il cane?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Ricordati di dargli da mangiare',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 16:15:22',
+                    text: 'Tutto fatto!',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Fabio',
                 avatar: '_2',
-                active: false,
+                visible: true,
+                messages: [{
+                    date: '20/03/2020 16:30:00',
+                    text: 'Ciao come stai?',
+                    status: 'sent'
+                },
+                {
+                    date: '20/03/2020 16:30:55',
+                    text: 'Bene grazie! Stasera ci vediamo?',
+                    status: 'received'
+                },
+                {
+                    date: '20/03/2020 16:35:00',
+                    text: 'Mi piacerebbe ma devo andare a fare la spesa.',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Samuele',
                 avatar: '_3',
-                active: false,
+                visible: true,
+                messages: [{
+                    date: '28/03/2020 10:10:40',
+                    text: 'La Marianna va in campagna',
+                    status: 'received'
+                },
+                {
+                    date: '28/03/2020 10:20:10',
+                    text: 'Sicuro di non aver sbagliato chat?',
+                    status: 'sent'
+                },
+                {
+                    date: '28/03/2020 16:15:22',
+                    text: 'Ah scusa!',
+                    status: 'received'
+                }
+                ],
             },
             {
                 name: 'Luisa',
                 avatar: '_4',
-                active: false,
+                visible: true,
+                messages: [{
+                    date: '10/01/2020 15:30:55',
+                    text: 'Lo sai che ha aperto una nuova pizzeria?',
+                    status: 'sent'
+                },
+                {
+                    date: '10/01/2020 15:50:00',
+                    text: 'Si, ma preferirei andare al cinema',
+                    status: 'received'
+                }
+                ],
             },
-        ]
+        ],
     },
     methods: {
-
+    setActive(index) {
+        this.currentIndex = index;
+    }
     },
 });
